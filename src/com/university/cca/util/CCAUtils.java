b431056dfe12a23eafe145ce415d893948e45243
Konstantin Rusev <k.russeff@abv.bg>
2021-02-16 07:19:21 +0200

    Add logback library to be able to log messages to the console and to a file. Update README file. Configure logback to be able to log messages to the console and to the file, and to rotate and zip the log files on daily basis.

--- src/com/university/cca/util/CCAUtils.java
+++ src/com/university/cca/util/CCAUtils.java
@@ -9,12 +9,16 @@ import javax.swing.JMenuItem;
 import com.university.cca.constants.Constants;
 
 /**
- * Utility methods for CCA Editor application
+ * Base utility class with utility methods for CCA Editor application
  * 
  * @author Konstantin Rusev
  * @version 1.0
  */
 public class CCAUtils {
+	
+	public CCAUtils() {
+		// Constructor that prevents creating an object of type CCAUtils
+	}
 	
 	public Dimension getScreenSize() {
 		return Toolkit.getDefaultToolkit().getScreenSize();
