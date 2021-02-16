f57a260e01ad43d65fc73e9ca58c5755d6221f0c
Konstantin Rusev <k.russeff@abv.bg>
2021-02-16 10:29:43 +0200

    Remove unnecessary menu items.

--- src/com/university/cca/main/Main.java
+++ src/com/university/cca/main/Main.java
@@ -1,16 +1,12 @@
 package com.university.cca.main;
 
 import javax.swing.ImageIcon;
-import javax.swing.JFrame;
 import javax.swing.JMenu;
-import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
 import com.university.cca.constants.Constants;
 import com.university.cca.frames.AppMainFrame;
 import com.university.cca.loading.LoadingScreen;
-import com.university.cca.menu.MainMenu;
-import com.university.cca.menu.MainMenuBar;
 import com.university.cca.menu.items.AboutMenuItems;
 import com.university.cca.menu.items.HelpMenuItems;
 
@@ -31,18 +27,7 @@ public class Main {
 		logger.info("CCA Editor application starting... :)");
 		createLoadingScreen();
 
-		AppMainFrame mainFrame = new AppMainFrame();
-		
-		// JMenuBar jMainMenuBar = new JMenuBar();
-		// createMainMenuBar(jMainMenuBar, mainFrame);
-
-		// TODO Main Menu - Work in progress
-		// JMenu file = new JMenu(Constants.FILE);
-		// JMenu edit = new JMenu(Constants.EDIT);
-		// JMenu view = new JMenu(Constants.VIEW);
-		// JMenu help = new JMenu(Constants.HELP);
-		// JMenu about = new JMenu(Constants.ABOUT);
-		// createMainMenu(jMainMenuBar, file, edit, view, help, about);
+		new AppMainFrame();
 		
 		// JMenuItem aboutCCA = new JMenuItem(Constants.ABOUT_CCA, getAboutIconImage());
 		// JMenuItem aboutCCAEditor = new JMenuItem(Constants.ABOUT_CCA_EDITOR, getAboutIconImage());
@@ -56,17 +41,6 @@ public class Main {
 	public static void createLoadingScreen() {
 		LoadingScreen loadingScreen = new LoadingScreen();
 		loadingScreen.createImage();
-	}
-
-	public static void createMainMenuBar(JMenuBar jMainMenuBar, JFrame jMainFrame) {
-		MainMenuBar mainMenuBar = new MainMenuBar();
-		mainMenuBar.createMainMenuBar(jMainMenuBar, jMainFrame);
-	}
-	
-	public static void createMainMenu(JMenuBar jMainMenuBar, JMenu file, JMenu edit,
-									  JMenu view, JMenu help, JMenu about) {
-		MainMenu mainMenu = new MainMenu();
-		mainMenu.createMainMenu(file, edit, view, help, about, jMainMenuBar);
 	}
 	
 	public static ImageIcon getAboutIconImage() {
