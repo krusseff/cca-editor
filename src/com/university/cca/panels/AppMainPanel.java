67272ade226ec45ae0fcfa13b1a3fd665f75df52
Konstantin Rusev <k.russeff@abv.bg>
2021-03-02 08:19:03 +0200

    Add image of the Plovdiv map to the application main panel.

--- src/com/university/cca/panels/AppMainPanel.java
+++ src/com/university/cca/panels/AppMainPanel.java
@@ -1,7 +1,9 @@
 package com.university.cca.panels;
 
-import java.awt.BorderLayout;
+import java.awt.Graphics;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -9,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.university.cca.buttons.CreateMessageButton;
+import com.university.cca.constants.Constants;
 
 /**
  * Main panel of the application
@@ -20,27 +23,35 @@ public class AppMainPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
     private static final Logger logger = LoggerFactory.getLogger(AppMainPanel.class);
+    
+    private static final int X_COORDINATE = 0;
+    private static final int Y_COORDINATE = 0;
 	
 	private JFrame parentFrame;
 	
 	public AppMainPanel(JFrame parentFrame) {
-		logger.info("Application main frame initialized");
+		logger.info("Application main panel with the map is initialized successfully");
 		this.parentFrame = parentFrame;
-		
-		// TODO
-		// this.setLayout(new CardLayout());
-		// PanelsUtil.setPanelSize(this);
 		
 		addContentToPanel();
 		
-		parentFrame.add(this, BorderLayout.CENTER); 
+		parentFrame.add(this);
 		this.setVisible(true);
 	}
 	
 	private void addContentToPanel() {
 		this.add(new CreateMessageButton(parentFrame));
 	}
-
+	
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        Image image = new ImageIcon(Constants.BACKGROUND_ICON_PATH).getImage();
+        
+        g.drawImage(image, X_COORDINATE, Y_COORDINATE, getWidth(), getHeight(), this);
+    }
+	
 	// Getters and setters
 	public JFrame getParentFrame() {
 		return this.parentFrame;
