9ecb13a26b86632d5c460a5b748ced9bf6cbf5ac
Konstantin Rusev <k.russeff@abv.bg>
2021-03-22 10:17:01 +0200

    Implement custom exception for the not found ambient message. Implement utility method to find a specific ambient message.

--- src/com/university/cca/generator/GeneratorUtil.java
+++ src/com/university/cca/generator/GeneratorUtil.java
@@ -1,11 +1,34 @@
 package com.university.cca.generator;
 
+import java.util.List;
+
+import com.university.cca.entities.Message;
+import com.university.cca.exceptions.AmbientMessageNotFound;
+
 public class GeneratorUtil {
 	
 	private static final String CSV_DELIMITER = ",";
 
 	private GeneratorUtil() {
 		// Prevent creating an object of type GeneratorUtil
+	}
+	
+	/**
+	 * Finds recipient ambient message from the file with all available messages
+	 */
+	protected static Message findMessageRecipient(List<Message> ambientMessages,
+									 			  Message message) throws AmbientMessageNotFound {
+		String errorMsg = "Ambient message not found! Message: " + message.toString();
+		
+		return ambientMessages
+			.stream()
+			.filter(msg ->
+				msg.getSenderAmbient().equals(message.getRecipientAmbient()) &&
+				msg.getRecipientAmbient().equals(message.getSenderAmbient()) &&
+				msg.getRespondToMessage().equals(message.getMessage())
+			)
+			.findFirst()
+			.orElseThrow(() -> new AmbientMessageNotFound(errorMsg));
 	}
 
 	/**
