4cdd328a1fbbc42b1b824aec05031f5ffabfb868
Konstantin Rusev <k.russeff@abv.bg>
2021-03-29 10:33:03 +0300

    Implement open CCA file dialog with an option to edit it.

--- src/com/university/cca/buttons/cca/OpenCCAFileButton.java
+++ src/com/university/cca/buttons/cca/OpenCCAFileButton.java
@@ -11,7 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.university.cca.constants.Constants;
-import com.university.cca.dialogs.UnderConstructionDialog;
+import com.university.cca.dialogs.cca.generation.OpenCCAFileDialog;
 import com.university.cca.util.MouseCursorUtil;
 
 /**
@@ -36,8 +36,6 @@ public class OpenCCAFileButton extends JButton implements ActionListener {
         this.setCursor(MouseCursorUtil.getMouseHand());
         this.setIconTextGap(Constants.ICON_GAP_SIZE);
         
-        this.setEnabled(false);
-        
         this.addActionListener(this);
     }
 	
@@ -45,8 +43,7 @@ public class OpenCCAFileButton extends JButton implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		logger.info("Open the CCA file button is clicked");
 
-		// TODO: Exact implementation here
-		new UnderConstructionDialog(parentFrame);
+		new OpenCCAFileDialog(getParentFrame());
 	}
 	
 	// Getters and Setters
