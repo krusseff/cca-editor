96de7326416472b592e3fac0a1f093d57299fb76
Konstantin Rusev <k.russeff@abv.bg>
2021-05-27 08:31:10 +0300

    Implement basic implementation of the show all messages dialog in a table as part of the View menu item.

--- src/com/university/cca/dialogs/menu/view/ShowMessagesDialog.java
+++ src/com/university/cca/dialogs/menu/view/ShowMessagesDialog.java
@@ -1,10 +1,18 @@
 package com.university.cca.dialogs.menu.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.swing.BoxLayout;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
+import com.university.cca.entities.Message;
+import com.university.cca.files.csv.AmbientCSVReader;
+import com.university.cca.tables.MessageTableModel;
 import com.university.cca.util.CCAUtils;
 
 /**
@@ -19,11 +27,14 @@ public class ShowMessagesDialog extends JDialog {
 	private static final long serialVersionUID = 1L;
 	
 	private static final String TITLE = "Show All Ambient Messages Table Dialog";
+	
+	private static final String NULL_VALUE = "null";
+	private static final String DASH_VALUE = "-";
+	
 	private static final boolean IS_VISIBLE = true;
 	private static final boolean IS_MODAL = true;
 	
-	// TODO: Think about the size of the dialog???
-	private static final int HEIGHT_DIALOG = CCAUtils.getScreenSize().height;
+	private static final int HEIGHT_DIALOG = CCAUtils.getScreenSize().height - 100;
 	private static final int WIDHT_DIALOG = CCAUtils.getScreenSize().width;
 	
 	private JFrame parentFrame;
@@ -44,10 +55,47 @@ public class ShowMessagesDialog extends JDialog {
 		JPanel dialogPanel = new JPanel();
         dialogPanel.setLayout(new BoxLayout(dialogPanel, BoxLayout.Y_AXIS));
         
-		// TODO: Add dialog content: Messages Table
-        // dialogPanel.add(createMessagesTable());
+        dialogPanel.add(createMessagesTable());
 
         this.getContentPane().add(dialogPanel);
+	}
+	
+	private JScrollPane createMessagesTable() {
+		List<Message> messages = getAmbientMessages();
+		
+		MessageTableModel messageTableModel = new MessageTableModel(messages);
+		JTable messageTable = new JTable(messageTableModel);
+		
+		// enable column sorting of the ambient messages table
+		messageTable.setAutoCreateRowSorter(MessageTableModel.IS_SORT_AVAILABLE);
+		
+		return new JScrollPane(messageTable);
+	}
+	
+	private List<Message> getAmbientMessages() {
+		return AmbientCSVReader.getAllMessages().stream()
+			.map(message ->
+			
+				new Message(
+					message.getSenderAmbient(),
+					message.getRecipientAmbient(), 
+					getValidRespondTo(message.getRespondToMessage()),
+					message.getMessage()
+				)
+			)
+			.collect(Collectors.toList());
+	}
+	
+	private String getValidRespondTo(String respondTo) {
+		String respondToResult;
+		
+		if (respondTo.equalsIgnoreCase(NULL_VALUE)) {
+			respondToResult = DASH_VALUE;
+		} else {
+			respondToResult = respondTo;
+		}
+		
+		return respondToResult;
 	}
 	
 	// Getters
