fd52b6bbf573aecb4ca2f941adf250be4b6e7314
Konstantin Rusev <k.russeff@abv.bg>
2021-06-09 11:35:44 +0300

    Implement a class with the different application modes.

---
+++ src/com/university/cca/constants/ApplicationModes.java
@@ -0,0 +1,18 @@
+package com.university.cca.constants;
+
+/**
+ * All constants for CCA Editor application modes
+ * 
+ * @author Konstantin Rusev
+ * @version 1.0
+ */
+public class ApplicationModes {
+
+	// Application modes available into the CCA Editor
+	public static final int SMART_CITY = 0;
+	public static final int SMART_AGRICULTURE = 1;
+	
+	private ApplicationModes() {
+		// private constructor to hide the implicit public one
+	}
+}
