9bbaaba77a0d1cce770ca757277eb4f54e6baab8
Konstantin Rusev <k.russeff@abv.bg>
2021-11-26 07:57:47 +0200

    Added new CCA constants for some placeholders and files for version 2 of the CCA generation.

--- src/com/university/cca/constants/CCATemplates.java
+++ src/com/university/cca/constants/CCATemplates.java
@@ -8,16 +8,23 @@ package com.university.cca.constants;
  */
 public class CCATemplates {
 	
-	// CCA Templates files paths
-	public static final String AMBIENTS_DELIMITER_FILE_PATH = "./files/cca_templates/ambients_delimiter.cca";
+	// CCA Templates files paths version 1
+	public static final String AMBIENTS_DELIMITER_FILE_PATH 	  = "./files/cca_templates/ambients_delimiter.cca";
 	public static final String SEND_RECEIVE_ONE_AMBIENT_FILE_PATH = "./files/cca_templates/send_receive_one_ambient.cca";
 	public static final String RECEIVE_SEND_ONE_AMBIENT_FILE_PATH = "./files/cca_templates/receive_send_one_ambient.cca";
 	
+	// CCA Templates files paths version 2
+	public static final String SEND_RECEIVE_AMBIENT_FILE_PATH 	  = "./files/cca_templates/send_receive_ambient.cca";
+	public static final String SEND_TO_SIBLING_AMBIENT_FILE_PATH  = "./files/cca_templates/send_to_sibling.cca";
+	public static final String RECEIVE_FROM_SIBLING_FILE_PATH	  = "./files/cca_templates/receive_from_sibling.cca";
+	
 	// CCA Templates placeholders
-	public static final String AMBIENT_SENDER_PLACEHOLDER = "AMBIENT_SENDER";
+	public static final String AMBIENT_SENDER_PLACEHOLDER 	  = "AMBIENT_SENDER";
 	public static final String AMBIENT_RECEIPIENT_PLACEHOLDER = "AMBIENT_RECEIPIENT";
-	public static final String SEND_PARAMETERS_PLACEHOLDER = "SEND_PARAMETERS";
+	public static final String SEND_PARAMETERS_PLACEHOLDER 	  = "SEND_PARAMETERS";
 	public static final String RECEIVE_PARAMETERS_PLACEHOLDER = "RECEIVE_PARAMETERS";
+	
+	public static final String SEND_RECEIVE_MESSAGES_PLACEHOLDER = "SEND_RECEIVE_MESSAGES_PLACEHOLDER";
 	
 	private CCATemplates() {
 		// private constructor to hide the implicit public one
