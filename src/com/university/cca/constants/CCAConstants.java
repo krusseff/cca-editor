8ea2ddfe33215be560ee07878d536e553cdb26a1
Konstantin Rusev <k.russeff@abv.bg>
2022-01-10 09:07:04 +0200

    Add some new constants.

--- src/com/university/cca/constants/CCAConstants.java
+++ src/com/university/cca/constants/CCAConstants.java
@@ -9,9 +9,10 @@ package com.university.cca.constants;
 public class CCAConstants {
 	
 	// CCA Generation constants
-	public static final String MSG_RESPOND_TO_NULL = "null";
-	public static final int CCA_STATUS_SUCCESS 	   = 0;
-	public static final int CCA_STATUS_FAILURE 	   = 1;
+	public static final String PASS_MESSAGE_TO_NULL = "null";
+	public static final String MSG_RESPOND_TO_NULL  = "null";
+	public static final int CCA_STATUS_SUCCESS 	    = 0;
+	public static final int CCA_STATUS_FAILURE 	    = 1;
 	
 	// CCA Templates files paths version 1
 	public static final String AMBIENTS_DELIMITER_FILE_PATH 	  = "./files/cca_templates/ambients_delimiter.cca";
